f95e7bfc1682289f6fed0b11517fe07a3a0a65ae
Jordan McQuiston <jordan@jordanmcquiston.com>
2019-10-29 23:34:38 -0700

    Final commit, I didn't commit eough on this one. Wont do that again! lol

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,55 @@
+var hour = "",
+    todayDate = null,
+    todayTime = null,
+    eventsArray = [],
+    timeArray = [];
 
-// select user input and save it in local storage when icon is clicked
-// if hour has passed disable input, else if time is current change to green else
-// turn orange
-
-
-var todayDate = null;
-var todayTime = null;
-var eventsArray = [];
-var objectArray = [];
-var timeEvent = [];
-
-
+// GETTING CURRENT DATE AND TIME AND DISPLAYING IT IN JUMBOTRON
 function todaysDateTime() {
-    todayDate = moment().format("MMMM Do, YYYY");
-    todayTime = moment().format("h:mm:ss a")
+  todayDate = moment().format("MMMM Do, YYYY");
+  todayTime = moment().format("h:mm:ss a");
+  hour = 12; //moment().format("HH");
+  for (var i = 9; i <= 17; i++) {
+    var textTimeId = $("." + i);
+    if (hour > i) {
+      textTimeId.addClass("past");
+      textTimeId.prop("disabled", true);
+    } else if (hour === i) {
+      textTimeId.addClass("now");
+    } else {
+      textTimeId.addClass("future");
+    }
+  }
   $("#todaysDate").text(todayDate);
   $("#todaysTime").text(todayTime);
 }
 
+// EVENT LISTENER FOR SAVE BUTTON AND STORING EVENT 
+// FROM TEXT BOX IN IN LOCALSTORAGE
 function storeEvent() {
-    $(".save").on("click", function(){
-        var saveBtn = $(this).attr("data-time");
-        var stringID = "#" + saveBtn;
-        var input = $(stringID).val().trim();
-        timeEvent = [{ time: saveBtn, event: input }];
-        console.log(timeEvent);
-        eventsArray.push(timeEvent);
-        console.log("no if " + eventsArray);
-    });
+  $(".save").on("click", function() {
+    timeEvent = [];
+    var saveBtn = $(this).attr("data-time");
+    var stringID = "#" + saveBtn;
+    var input = $(stringID)
+      .val()
+      .trim();
+    localStorage.setItem(saveBtn, input);
+  });
 }
 
+// LOOPING THROUGH LOCAL STORAGE AND DISPLAYING IN TEXTAREAS 
+function displayEvents() {
+  for (var i = 0; i < localStorage.length; i++) {
+    var key = localStorage.key(i);
+    var textArea = $("#" + key);
+    textArea.text(localStorage.getItem(key));
+  }
+}
 
-$(document).ready(function(){
-    todaysDateTime();
-    storeEvent();
-    setInterval(todaysDateTime, 1000);
+$(document).ready(function() {
+  todaysDateTime();
+  storeEvent();
+  displayEvents();
+  setInterval(todaysDateTime, 1000);
 });
-
-
